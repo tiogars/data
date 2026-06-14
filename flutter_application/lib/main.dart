f37f5a476816877fd09dfcb629b2f4674a91022c
tiogars <tiogars@gmail.com>
2026-06-14 12:40:09 +0200

    feat: ajouter le support de sqflite_common_ffi pour la gestion de la base de données sur Windows

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_application/app/app.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(const DataMobileApp());
 }
